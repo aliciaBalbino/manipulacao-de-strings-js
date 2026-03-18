63fa109a683827bb95b45ecb9f1e5115ae9b6ce6
aliciaBalbino <alicia.balbino@outlook.com>
2026-03-18 16:56:58 -0300

    Create index.js
    # Manipulação de Strings com JavaScript
    
    Este projeto demonstra como substituir palavras em uma frase usando JavaScript e expressões regulares (RegExp).
    
    ## Exemplo
    
    Entrada:
    Clique no anúncio e fale com nossos corretores.
    
    Saída:
    Clique no anúncio e fale com nossos consultores.

---
+++ index.js
@@ -0,0 +1,6 @@
+var frase = 'Clique no anúncio e fale com nossos corretores. Nossos Corretores são qualificados';
+
+var novaFrase = frase.replace(/corretores/gi, 'consultores');
+
+
+console.log(novaFrase);
